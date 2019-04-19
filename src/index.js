2d6a37233db938ced048875931fb3eba8e8aa45e
Adrian Bona <adrian.bonaccini@gmail.com>
2019-04-19 18:51:56 -0300

    Favicon was failing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 
+import './styles/index.css'
 import 'decentraland-ui/lib/styles.css'
 import 'decentraland-ui/lib/dark-theme.css'
 
